1e3f017e362ca30b626f95217f4e404562f9a828
wosh1ngton <woshington.rod@gmail.com>
2024-02-14 14:04:47 -0300

    home.component

--- cashcontroller-app/src/app/app.module.ts
+++ cashcontroller-app/src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  { path: '', component: HomeComponent }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
